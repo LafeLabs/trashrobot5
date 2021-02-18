35995219269ec2d099a1d09c21ec89cbb1a465be
Lafe Spietz <lafelabs@gmail.com>
2021-02-18 15:30:39 -0500

    smaller coin for default

--- arduinocode/oscillator/oscillator.ino
+++ arduinocode/oscillator/oscillator.ino
@@ -11,7 +11,11 @@ OSCILLATOR
 int feedback = 0;
 int x = 512;
 int x0 = 512;
-int drivePin  = 7;
+int drivePin  = 5;
+int counter = 0;
+int triggertime = 150;//ms
+boolean coilon = false;
+int delaytime = 10;//ms
 
 void setup() {
   Serial.begin(115200);
@@ -22,17 +26,32 @@ void setup() {
  }
 
 void loop() {
+  counter++;
   feedback = analogRead(A0);  
   x = x0 - feedback;
   if(x > 3){
      digitalWrite(drivePin,HIGH);
+     coilon = true;
   }
   else{
      digitalWrite(drivePin,LOW);    
+     coilon = false;
+  }
+
+  if(counter*delaytime > triggertime){
+    if(coilon == true){
+     digitalWrite(drivePin,LOW);    
+     coilon = false;
+    }
+    else{
+     digitalWrite(drivePin,HIGH);
+     coilon = true;      
+    }
+    counter = 0;
   }
  
  Serial.println(x); 
  
-  delay(10);
+  delay(delaytime);//10 ms
   
 }
